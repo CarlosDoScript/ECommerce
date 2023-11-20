eadf6cee3a509ac46791623002229692b8c37a71
Carlos Henrique <carlosmvsep3@hotmail.com>
2023-11-20 11:50:11 -0300

    Implementando o CRUD de cliente no endpoint ja fazendo direcionamento para o repository

--- API.ECommerceLambda/src/API.ECommerceLambda/Controllers/ClienteController.cs
+++ API.ECommerceLambda/src/API.ECommerceLambda/Controllers/ClienteController.cs
@@ -1,4 +1,5 @@
 using API.ECommerceLambda.Models;
+using API.ECommerceLambda.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.ECommerceLambda.Controllers
@@ -8,34 +9,40 @@ namespace API.ECommerceLambda.Controllers
     public class ClienteController : ControllerBase
     {
         private readonly ILogger<ClienteController> _logger;
+        private readonly IClienteRepository _repository;
 
-        public ClienteController(ILogger<ClienteController> logger)
+        public ClienteController(ILogger<ClienteController> logger, IClienteRepository repository)
         {
             _logger = logger;
+            _repository = repository;
         }
 
         [HttpPost]
         public async Task<IActionResult> CriarCliente(Cliente cliente)
         {
+            await _repository.Adicionar(cliente);
             return Ok();
         }
 
         [HttpPut]
         public async Task<IActionResult> AtualizarCliente(Cliente cliente)
         {
+            await _repository.Atualizar(cliente);
             return Ok();
         }
 
         [HttpDelete("{documento}")]
         public async Task<IActionResult> DeletarCliente(string documento)
         {
+            await _repository.Deletar(documento);
             return Ok();
         }
 
         [HttpGet("{documento}")]
         public async Task<IActionResult> ObterClientePorDocumento(string documento)
         {
-            return Ok();
+            var cliente = await _repository.Buscar(documento);
+            return Ok(cliente);
         }
     }
 }
