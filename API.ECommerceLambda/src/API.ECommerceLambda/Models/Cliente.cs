9d691c4348157312756cd07e22f087e56ab7042c
Carlos Henrique <carlosmvsep3@hotmail.com>
2023-11-20 10:06:33 -0300

    Criação dos modelos de entidade da aplicação

---
+++ API.ECommerceLambda/src/API.ECommerceLambda/Models/Cliente.cs
@@ -0,0 +1,10 @@
+﻿namespace API.ECommerceLambda.Models
+{
+    public class Cliente
+    {
+        public string? Documento { get; set; }
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
+        public Endereco? Enderecp {  get; set; }
+    }
+}
---
+++ API.ECommerceLambda/src/API.ECommerceLambda/Models/Endereco.cs
@@ -0,0 +1,11 @@
+﻿namespace API.ECommerceLambda.Models
+{
+    public class Endereco
+    {
+        public string? Logradouro { get; set; }
+        public int Numero { get; set; }
+        public string? Complemento { get; set; }
+        public string? Cidade { get; set; }
+        public string? Estado { get; set; }
+    }
+}
