91aafc56acd58aee165c4c3efdee1b22b15943a4
Carlos Henrique <carlosmvsep3@hotmail.com>
2023-11-20 12:23:52 -0300

    Arrumando o nome dado ao endereco na entidade de cliente

--- API.ECommerceLambda/src/API.ECommerceLambda/Models/Cliente.cs
+++ API.ECommerceLambda/src/API.ECommerceLambda/Models/Cliente.cs
@@ -9,6 +9,6 @@ namespace API.ECommerceLambda.Models
         public string? Documento { get; set; }
         public string? Nome { get; set; }
         public string? Email { get; set; }
-        public Endereco? Enderecp {  get; set; }
+        public Endereco? Endereco {  get; set; }
     }
 }
